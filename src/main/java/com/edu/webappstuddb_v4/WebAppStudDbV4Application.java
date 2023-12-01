f7c4ae6b9477d125beb43b632a58e03f20e58a97
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-12-01 23:51:01 +0200

    HelloWorld init commit

--- src/main/java/com/edu/webappstuddb_v4/WebAppStudDbV4Application.java
+++ src/main/java/com/edu/webappstuddb_v4/WebAppStudDbV4Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebAppStudDbV4Application {
-
     public static void main(String[] args) {
         SpringApplication.run(WebAppStudDbV4Application.class, args);
     }
