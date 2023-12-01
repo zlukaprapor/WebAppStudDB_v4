620cf77302f9be39e164b412406947ab7af969f2
Oleksii Tkachenko <zlukapraporr@gmail.com>
2023-12-01 23:46:19 +0200

    HelloWorld

---
+++ src/main/java/com/edu/webappstuddb_v4/WebAppStudDbV4Application.java
@@ -0,0 +1,13 @@
+package com.edu.webappstuddb_v4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppStudDbV4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppStudDbV4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/webappstuddb_v4/WebAppStudDbV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.webappstuddb_v4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppStudDbV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
